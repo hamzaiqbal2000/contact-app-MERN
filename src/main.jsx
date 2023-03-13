9f02743fad65818e8f54480fac66b53a7b7ee5c0
hamza12022skipq <hiqbal.bscs18seecs@seecs.edu.pk>
2023-03-13 23:50:42 +0500

    fetched data for userDetails using react-query

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import store from "./store"
-import { Provider } from "react-redux"
-import { BrowserRouter as Router } from "react-router-dom"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import store from "./store";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Create a client
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Provider store={store}>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </Provider>
     </Router>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
