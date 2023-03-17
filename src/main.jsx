6d8ef2536606f2a4e46d7438f670df15729f8727
hamza12022skipq <hiqbal.bscs18seecs@seecs.edu.pk>
2023-03-17 22:49:24 +0500

    added code for invalidateQueries

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,8 @@ import App from "./App";
 import store from "./store";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-// Create a client
-const queryClient = new QueryClient();
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "./query-client-provider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,40 @@
 import React, { useState } from "react";
 import CardList from "../containers/CardList/CardList";
 import ContactForm from "../containers/ContactForm/ContactForm";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import queryClient from "../query-client-provider";
 
 const Home = () => {
   const [toggle, setToggle] = useState(false);
+  const { id, name, email, phone } = useSelector((state) => ({
+    id: state.formReducer.id,
+    name: state.formReducer.name,
+    email: state.formReducer.email,
+    phone: state.formReducer.phone,
+  }));
+
+  const mutation = useMutation({
+    mutationFn: () => {
+      return axios.post(`http://localhost:4000/addUsers`, {
+        id: id,
+        name: name,
+        email: email,
+        phone: phone,
+      });
+    },
+    onSuccess: (successData) => {
+      console.log("successData", successData);
+      queryClient.invalidateQueries("formDataDetail");
+    },
+  });
 
   return (
     <>
       <section className="container-fluid row mt-2">
-        <ContactForm toggle={toggle} setToggle={setToggle} />
-        <CardList toggle={toggle} />
+        <ContactForm mutation={mutation} />
+        <CardList />
       </section>
     </>
   );
